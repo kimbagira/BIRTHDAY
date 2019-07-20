dc0eaa21dce3435c997ccd94ca941e48da9c6ddf
kimbagira <sylviekimbagira@gmail.com>
2019-07-20 13:59:07 +0200

    css/styles.css

--- js/script.js
+++ js/script.js
@@ -6,28 +6,31 @@ function put(){
     var dayoftheweek = (cc/4 -2*cc-1 + 5*yy/4 + 26*(mm+1)/10 + dd)%7;
     console.log( Math.round(dayoftheweek));
     alert(Math.round(dayoftheweek));
-    var array1=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    var array2=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var array1=["nul","Adwoa","Abenaa","Akua","Yaa","Afua","Ama","Akosua"];
+    var array2=["nul","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame","Kwasi"];
   
     var gender = document.querySelector('input[name="gender"]:checked').value;
+
+     if (dd<=0 || dd>31) {
+        alert("enter a valid day");  
+      
+      }
+      else if (mm<= 0 || mm> 12)  {
+        alert("enter a valid month");  
+      }
     
-    if (gender==="female") {
+      else if (gender==="female") {
         alert(gender);
         console.log(array1[Math.round(dayoftheweek)]);
         document.getElementById("answer").innerHTML = array1[Math.round(dayoftheweek)];
     }
-    else if (gender==="male"){
+    else if(gender==="male"){
+         alert(gender);
         console.log(array2[Math.round(dayoftheweek)]);
-        document.getElementById("answer").innerHTML = array1[Math.round(dayoftheweek)];
+        document.getElementById("answer").innerHTML = array2[Math.round(dayoftheweek)];
     }
-      else if (dd<=0 || dd>31) {
-      
-      }
-      else if (m<= 0 || m > 12)  {
-
-      }
+     
       else{
           console.log("done");
       }
-
-}
+    }
